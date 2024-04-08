fce76b37891640ed2ad0add605e4bbc3194a13f7
Emily Richmond <emrichmond@alaska.edu>
2024-04-08 15:03:52 -0800

    add basic plot and table for hover function

--- app/app.R
+++ app/app.R
@@ -1,12 +1,13 @@
 library(shiny)
 library(bslib)
-# library(readr)
-# library(dplyr)
-# library(ggplot2)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(DT)
 
 #data
-# source <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/prices/weighted_prices.csv"
-# weighted_prices <- read_csv(url(source))
+source <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/prices/weighted_prices.csv"
+weighted_prices <- read_csv(url(source))
 
 # Define UI for application that draws a histogram
 ui <- page_sidebar(
@@ -16,15 +17,32 @@ ui <- page_sidebar(
                 label = "Select Region",
                 choices = c("Coastal","Railbelt","Rural Remote"),
                 selected = "Coastal")
-
-  )
-
+  ),
+  card(
+    plotOutput(outputId = "line_plot", hover = "plot_hover"),
+    dataTableOutput(outputId = "hover_table")
+  )#,
+  # card(dataTableOutput(outputId = "hover_table")
+  # )
 )
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+  regional_subset <- reactive({
+    weighted_prices %>%
+      filter(acep_energy_region == input$acep_energy_region)
+  })
+
+  output$line_plot <- renderPlot({
+    ggplot(regional_subset(), aes(x = year, y = weighted_price, colour = sector)) +
+      geom_line()
+})
+
+  output$hover_table <- renderDataTable({
+    nearPoints(regional_subset(), input$plot_hover)
+  })
 }
 # Run the application
 shinyApp(ui = ui, server = server)
