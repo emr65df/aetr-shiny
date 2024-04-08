ceed5a3379ed75a09bf0457d414df6a6266b8047
Emily Richmond <emrichmond@alaska.edu>
2024-04-08 14:48:24 -0800

    add selectInput for region

--- app/app.R
+++ app/app.R
@@ -1,27 +1,30 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(bslib)
-library(readr)
+# library(readr)
+# library(dplyr)
+# library(ggplot2)
 
-source <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/prices/weighted_prices.csv"
-weighted_prices <- read_csv(url(source))
+#data
+# source <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/prices/weighted_prices.csv"
+# weighted_prices <- read_csv(url(source))
+
 # Define UI for application that draws a histogram
 ui <- page_sidebar(
+  title = "Prices of Electricity",
+  sidebar = sidebar(
+    selectInput(inputId = "acep_energy_region",
+                label = "Select Region",
+                choices = c("Coastal","Railbelt","Rural Remote"),
+                selected = "Coastal")
+
+  )
 
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
 }
-
 # Run the application
 shinyApp(ui = ui, server = server)
