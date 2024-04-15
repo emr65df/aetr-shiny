edb9771bef7562a0041f379489f5c72bbf3f535d
Emily Richmond <emrichmond@alaska.edu>
2024-04-15 14:03:37 -0800

    first version of all graphs complete

--- aetr-app/app.R
+++ aetr-app/app.R
@@ -7,12 +7,14 @@ library(plotly)
 library(kableExtra)
 source("helpers.R")
 
+
 #installed capacity data
 source_capacity <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/capacity/capacity_long.csv"
 capacity <- read_csv(url(source_capacity))
 #filter data prime_mover != NA
 capacity_filtered <- capacity %>%
-  filter(!is.na(prime_mover))
+  filter(!is.na(prime_mover)) %>%
+  mutate(across(where(is.numeric), round, 2))
 
 #generation
 source_generation <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/generation/net_generation_long.csv"
@@ -21,13 +23,18 @@ generation <- read_csv(url(source_generation))
 #consumption
 source_consumption <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/consumption/consumption_long.csv"
 consumption <- read_csv(url(source_consumption))
+#filter data <= 2019
+consumption_filtered <- consumption %>%
+  filter(year <= 2019) %>%
+  mutate(across(where(is.numeric), round, 2))
 
 #prices data
 source_weighted_prices <- "https://raw.githubusercontent.com/acep-uaf/aetr-web-book-2024/main/data/working/prices/weighted_prices.csv"
 weighted_prices <- read_csv(url(source_weighted_prices))
-#filter data < 2020
+#filter data <= 2019
 weighted_prices_filtered <- weighted_prices %>%
-  filter(year <= 2019)
+  filter(year <= 2019) %>%
+  mutate(across(where(is.numeric), round, 2))
 
 # Define UI for application that draws a histogram
 ui <- page_navbar(
@@ -44,6 +51,15 @@ ui <- page_navbar(
     ),
     #tags$a("2024 Alaska Electricity", tags$br(), "Trends Report", href = "https://acep-uaf.github.io/aetr-web-book-2024/")
   #   downloadButton("download_data", "All Regions")
+    nav_panel("About", div(p("The purpose of this application is to showcase the 2024 Alaska
+                              Electricity Trends Report graphs in a different format. Shiny Applications
+                              are incredibly flexible, customizable, and allow for much more interactivity than
+                              what is demonstrated here. Click some tabs, stay awhile, and hover around to explore the details of each plot.\n
+                              The link below will take you to the web book."),
+                           tags$a("2024 Alaska Electricity", tags$br(), "Trends Report", href = "https://acep-uaf.github.io/aetr-web-book-2024/"),
+                           ),
+              div(style = "position: absolute; bottom: 10px; right: 8%;",
+                  p("Created by Emily Richmond"))),
     # Panel with plot ----
     nav_panel("Installed Capacity", plotlyOutput(outputId = "ic_plot")),
 
@@ -51,12 +67,17 @@ ui <- page_navbar(
     nav_panel("Net/Gross Generation", plotlyOutput(outputId = "ng_plot")),
 
     # Panel with table ----
-    nav_panel("Consumption and Sales", plotOutput(outputId = "cs_plot")),
+    nav_panel("Consumption and Sales", plotlyOutput(outputId = "cs_plot")),
     nav_panel("Price of Electricity", plotlyOutput(outputId = "pe_plot"))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+
+  # output$about <- renderText({p("The purpose of this application to showcase the 2024 Alaska
+  #                   Electricity Trends Report in a different format. Shiny Applications
+  #                   are incredibly flexible and customizable.")
+  # })
 
 plot_theme <- theme(axis.title.y = element_text(angle=0, size = 7, colour = "white"),
                     axis.title.x = element_text(size = 7, colour = "white", hjust = 1),
@@ -76,7 +97,7 @@ plot_theme <- theme(axis.title.y = element_text(angle=0, size = 7, colour = "whi
     if (c("Statewide") %in% input$acep_energy_region)
       capacity_filtered %>%
       group_by(year, prime_mover) %>%
-      summarize(capacity = sum(capacity))  %>%
+      summarize(capacity = sum(capacity)) %>%
       arrange(capacity, .by_group = TRUE) %>%
       mutate(prime_mover = factor(prime_mover, levels = unique(prime_mover)))
     else
@@ -107,7 +128,8 @@ plot_theme <- theme(axis.title.y = element_text(angle=0, size = 7, colour = "whi
     if (c("Statewide") %in% input$acep_energy_region)
     generation %>%
       group_by(year, acep_region) %>%
-      summarize(generation = sum(generation, na.rm = T)/1000)
+      summarize(generation = sum(generation, na.rm = T)/1000) %>%
+      mutate(across(where(is.numeric), round, 2))
     else
     generation %>%
       filter(acep_region == input$acep_energy_region &
@@ -118,7 +140,8 @@ plot_theme <- theme(axis.title.y = element_text(angle=0, size = 7, colour = "whi
       group_by(year, fuel_type) %>%
       summarise(generation = sum(generation, na.rm = T)/1000) %>%
       arrange(generation, .by_group = TRUE) %>%
-      mutate(fuel_type = factor(fuel_type, levels = unique(fuel_type)))
+      mutate(fuel_type = factor(fuel_type, levels = unique(fuel_type))) %>%
+      mutate(across(where(is.numeric), round, 2))
   })
 
   output$ng_plot <- renderPlotly({
@@ -140,6 +163,63 @@ plot_theme <- theme(axis.title.y = element_text(angle=0, size = 7, colour = "whi
           layout(legend = list(title = ""),
                  hovermode = "x unified")
     })
+
+
+  cs_subset <- reactive({
+    if (c("Statewide") %in% input$acep_energy_region)
+      consumption_filtered %>%
+      filter(class %in% c("Residential", "Commercial", "Other")) %>%
+      group_by(year, class) %>%
+      summarise(
+        "revenue" = sum(revenue, na.rm = TRUE),
+        "sales" = sum(sales, na.rm = TRUE),
+        "customers" = sum(customers, na.rm = TRUE)) %>%
+      mutate(sales_per_capita = round((sales/customers),  2))
+    else
+      consumption_filtered %>%
+      filter(class %in% c("Residential", "Commercial", "Other"), acep_region == input$acep_energy_region) %>%
+      group_by(year, class) %>%
+      summarise(
+        "revenue" = sum(revenue, na.rm = TRUE)/1000,
+        "sales" = sum(sales, na.rm = TRUE)/1000,
+        "customers" = sum(customers, na.rm = TRUE))
+  })
+
+  output$cs_plot <- renderPlotly({
+    p <- ggplot(cs_subset(), aes(x = year, y = switch(input$acep_energy_region,
+                                                      "Statewide" = sales_per_capita,
+                                                      "Coastal" = sales,
+                                                      "Rural Remote" = sales,
+                                                      "Railbelt" = sales), fill = class,
+                                 text = switch(input$acep_energy_region,
+                                               "Statewide" = paste("Sales per Capita (MWh):", sales_per_capita),
+                                               "Coastal" = paste("Sales (GWh):", sales),
+                                               "Rural Remote" = paste("Sales (GWh):", sales),
+                                               "Railbelt" = paste("Sales (GWh):", sales)))) +
+      geom_col(position = "stack") +
+      scale_x_continuous(breaks = seq(min(cs_subset()$year), max(cs_subset()$year), by = 1)) +
+      scale_y_continuous(limits = switch(input$acep_energy_region,
+                                         "Statewide" = c(0,400),
+                                         "Coastal" = c(0,5000),
+                                         "Rural Remote" = c(0,5000),
+                                         "Railbelt" = c(0,5000))) +
+      labs(title = switch(input$acep_energy_region,
+                          "Statewide" = paste(input$acep_energy_region, "Trends in Sales per Capita by Class"),
+                          "Coastal" = paste(input$acep_energy_region, "Trends in Sales by Class"),
+                          "Rural Remote" = paste(input$acep_energy_region, "Trends in Sales by Class"),
+                          "Railbelt" = paste(input$acep_energy_region, "Trends in Sales by Class")),
+           subtitle = "hover over points for details") +
+      ylab(switch(input$acep_energy_region,
+                  "Statewide" = "Sales per Capita (MWh)",
+                  "Coastal" = "Sales (GWh)",
+                  "Rural Remote" = "Sales (GWh)",
+                  "Railbelt" = "Sales (GWh)")) +
+      plot_theme
+
+    ggplotly(p, tooltip = "text") %>%
+      layout(legend = list(title = ""),
+             hovermode = "x unified")
+  })
 
   pe_subset <- reactive({
     if (c("Statewide") %in% input$acep_energy_region)
